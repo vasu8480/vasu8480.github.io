dd8d29f2bc93c78acb9a0636716b32cdb6e7c46e
vasu8480 <vasu8480@gmail.com>
2022-08-03 13:00:52 +0530

    vasurobofrnds

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+	{
+		id: 0,
+		name: 'Vasu',
+		username: 'vasu badg',
+		email: 'vasdu@wef'
+	},
+	{
+		id: 1,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'sincere@apa.vom'
+	},
+	{
+		id: 2,
+		name: 'Ervin Howell',
+		username: 'Antonette',
+		email: 'shann@fkasf.com'
+	},
+	{
+		id: 3,
+		name: 'Clementine Bauch',
+		username: 'Samantha',
+		email: 'Nathoad@gmail.com'
+	},
+	{
+		id: 4,
+		name: 'Patricia Lebsack',
+		username: 'Karianne',
+		email: 'af@af.com'
+	},
+	{
+		id: 5,
+		name: 'Chelsey Dietrich',
+		username: 'Kamren',
+		email: 'sdfas@safas'
+	},
+	{
+		id: 6,
+		name: 'Mrs. Dennis Schulist',
+		username: 'Leopoldo_Corkery',
+		email: 'sfd@erfa.vom'
+	}
+];
